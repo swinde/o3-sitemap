4729c4cb60f567507c360522c8a6d4a2dd475176
Steffen Winde <inserv@winde-ganzig.de>
2026-06-01 16:45:24 +0200

    Füge Einstellung für ausgeschlossene URLs zur Modulmetadaten hinzu

--- metadata.php
+++ metadata.php
@@ -16,7 +16,20 @@ $aModule          = [
     'extend'      => [],
     'blocks'      => [],
     'settings'    => [
-
-
+        [
+            'group' => 'main',
+            'name'  => 'swinde_sitemap_exclude',
+            'type'  => 'aarr',
+            'value' => '',
+            'label' => [
+                'de' => 'Ausgeschlossene URLs',
+                'en' => 'Excluded URLs',
+            ],
+            'hint'  => [
+                'de' => 'Geben Sie hier die URLs an, die von der Sitemap ausgeschlossen werden sollen. Jede URL sollte in einer neuen Zeile stehen.',
+                'en' => 'Enter the URLs to be excluded from the sitemap here. Each URL should be on a new line.',
+            ],
+        ],
+        
     ],
 ];
